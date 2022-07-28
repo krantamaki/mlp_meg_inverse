81d14132dca00c521ce1a09e23f29ff02f19a809
Kasper Rantamäki <kasper.rantamaki@gmail.com>
2022-07-28 18:04:09 +0300

    Decreasing computational complexity

--- optimization_run.sh
+++ optimization_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Make sure to request only the resources you really need to avoid cueing
-#SBATCH --time=168:00:00
-#SBATCH --mem-per-cpu=16G
+#SBATCH --time=150:00:00
+#SBATCH --mem-per-cpu=10G
 #SBATCH -n 1
 
 module load neuroimaging
