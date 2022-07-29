e829c4e07e9746992ed1b03a064eaafcc75c3597
krantamaki <78734468+krantamaki@users.noreply.github.com>
2022-07-29 08:18:32 +0300

    Update optimization_run.sh

--- optimization_run.sh
+++ optimization_run.sh
@@ -2,7 +2,7 @@
 
 # Make sure to request only the resources you really need to avoid cueing
 #SBATCH --time=150:00:00
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=4G
 #SBATCH -n 1
 
 module load neuroimaging
