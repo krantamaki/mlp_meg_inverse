d744fa1599f39cb6a061d90378981457d1d8aba5
krantamaki <78734468+krantamaki@users.noreply.github.com>
2022-07-29 08:20:19 +0300

    Update optimization_run.sh

--- optimization_run.sh
+++ optimization_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure to request only the resources you really need to avoid cueing
-#SBATCH --time=150:00:00
+#SBATCH --time=125:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH -n 1
 
